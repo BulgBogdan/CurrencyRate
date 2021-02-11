098e902a565ef2b5260c19df96099a3acd2fd848
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-02-11 19:07:19 +0300

    added scheduleConfig

---
+++ src/main/java/currencyRate/configuration/ScheduleConfig.java
@@ -0,0 +1,17 @@
+package currencyRate.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.destroy();
+    }
+
+}
