a3e6ac2791f0a66a10c49ac2a2be5a73b7964214
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-02-20 12:20:09 +0300

    added parser xml

---
+++ src/main/java/currencyRate/tasks/banks/ParserXml.java
@@ -0,0 +1,24 @@
+package currencyRate.tasks.banks;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public final class ParserXml {
+
+    public static XMLEventReader getReader(String url) {
+        XMLEventReader reader = null;
+        try {
+            URL inUrl = new URL(url);
+            InputStream input = inUrl.openStream();
+            XMLInputFactory factory = XMLInputFactory.newInstance();
+            reader = factory.createXMLEventReader(url, input);
+        } catch (XMLStreamException | IOException e) {
+            e.printStackTrace();
+        }
+        return reader;
+    }
+}
