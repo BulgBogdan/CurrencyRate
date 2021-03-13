bae0b76b53485efcdac926c020681a2870cd32ea
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-03-13 18:31:11 +0300

    added bsb

---
+++ src/main/java/currencyRate/tasks/component/BSB.java
@@ -0,0 +1,87 @@
+package currencyRate.tasks.component;
+
+import currencyRate.entity.*;
+import currencyRate.service.*;
+import currencyRate.tasks.banks.ParserBSB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Component
+@PropertySource("classpath:banks.properties")
+public class BSB {
+    @Value("${url.bsb}")
+    private String url;
+
+    private BankService bankService;
+    private BranchService branchService;
+    private CityService cityService;
+    private SelectService selectService;
+    private TypeService typeService;
+    private ValueService valueService;
+    private ValueAndBranch valueAndBranch;
+
+    @Autowired
+    public BSB(BankService bankService, BranchService branchService, CityService cityService,
+               SelectService selectService, TypeService typeService, ValueService valueService,
+               ValueAndBranch valueAndBranch) {
+        this.bankService = bankService;
+        this.branchService = branchService;
+        this.cityService = cityService;
+        this.selectService = selectService;
+        this.typeService = typeService;
+        this.valueService = valueService;
+        this.valueAndBranch = valueAndBranch;
+    }
+
+    public void createAndUpdateValues() {
+        Bank bank = bankService.getByName("БСБ Банк");
+
+        List<City> cities = cityService.getAll();
+        List<SelectCurrency> selectCurrencies = selectService.getAll();
+        List<TypeCurrency> typeCurrencies = typeService.getAll();
+        List<BankBranch> listFilials = branchService.getBranchesByBankId(bank.getId());
+
+        int i = 0;
+        if (!listFilials.isEmpty()) {
+            i = listFilials.get(0).getId();
+        }
+        for (City city : cities) {
+            for (TypeCurrency typeCurrency : typeCurrencies) {
+                for (SelectCurrency selectCurrency : selectCurrencies) {
+                    System.out.println("--------------------------nachalo " + LocalTime.now().toString());
+                    String value;
+                    if (city.getName().equalsIgnoreCase("минск")) {
+                        value = ParserBSB.getValue(typeCurrency.getName(), selectCurrency.getSelect(), url);
+                    } else {
+                        value = "Нет информации";
+                    }
+                    System.out.println("seredina " + LocalTime.now().toString());
+                    if (!listFilials.isEmpty()) {
+                        BankBranch branch = valueAndBranch.getEditBranch(branchService.getFilialById(i),
+                                ParserBSB.getBranch(city, bank));
+                        branchService.edit(branch);
+
+                        ValueCurrency valueCurrency = valueAndBranch.getEditValueCurrency(valueService.getById(i),
+                                valueAndBranch.getValueCurrency(branch, selectCurrency, typeCurrency, value));
+                        valueService.edit(valueCurrency);
+                    } else {
+                        BankBranch branch = ParserBSB.getBranch(city, bank);
+                        branchService.add(branch);
+
+                        ValueCurrency valueCurrency = valueAndBranch
+                                .getValueCurrency(branch, selectCurrency, typeCurrency, value);
+                        valueService.add(valueCurrency);
+                    }
+                    System.out.println("-----------------------------------------konec " + LocalTime.now().toString());
+                    i++;
+                }
+            }
+        }
+    }
+
+}
