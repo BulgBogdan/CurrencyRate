dc15a0cc0847e42368921cd59a41ef20ba552ba4
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-03-16 20:04:55 +0300

    added schedule

---
+++ src/main/java/currencyRate/tasks/scheduleds/ScheduledPost.java
@@ -0,0 +1,45 @@
+package currencyRate.tasks.scheduleds;
+
+import currencyRate.tasks.component.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class ScheduledPost {
+
+    private Belarusbank belarusbank;
+    private Belagrpombank belagrpombank;
+    private Belgazprombank belgazprombank;
+    private BSB bsb;
+    private BankDabrabyt dabrabyt;
+    private Absolutebank absolutebank;
+    private MTBank mtb;
+    private VTB vtb;
+
+    @Autowired
+    public ScheduledPost(Belarusbank belarusbank, Belagrpombank belagrpombank, Belgazprombank belgazprombank,
+                         BSB bsb, BankDabrabyt dabrabyt, Absolutebank absolutebank, MTBank mtb, VTB vtb) {
+        this.belarusbank = belarusbank;
+        this.belagrpombank = belagrpombank;
+        this.belgazprombank = belgazprombank;
+        this.bsb = bsb;
+        this.dabrabyt = dabrabyt;
+        this.absolutebank = absolutebank;
+        this.mtb = mtb;
+        this.vtb = vtb;
+    }
+
+    @PostConstruct
+    public void createFilialsWithValues() {
+        belarusbank.createAndUpdateValues();
+        belagrpombank.createAndUpdateValues();
+        belgazprombank.createAndUpdateValues();
+        bsb.createAndUpdateValues();
+        dabrabyt.createAndUpdateValues();
+        absolutebank.createAndUpdateValues();
+        mtb.createAndUpdateValues();
+        vtb.createAndUpdateValues();
+    }
+}
