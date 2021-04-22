cf80a382d631dffb4a72db787ecc106ef130426b
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-04-22 17:27:30 +0300

    add logic courses controller

--- src/main/java/currencyRate/controller/CoursesController.java
+++ src/main/java/currencyRate/controller/CoursesController.java
@@ -1,16 +1,92 @@
 package currencyRate.controller;
 
+import currencyRate.controller.utils.CoursesUtil;
+import currencyRate.entity.*;
+import currencyRate.service.*;
+import currencyRate.tasks.banks.NationalBank;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 public class CoursesController {
 
     private ModelAndView modelAndView = new ModelAndView();
 
-    @GetMapping("/courses")
-    public ModelAndView coursesPage() {
+    private CityService cityService;
+
+    private ValueService valueService;
+
+    private TypeService typeService;
+
+    private BankService bankService;
+
+    private BranchService branchService;
+
+    private SelectService selectService;
+
+    private NationalBank nationalBank;
+
+    @Autowired
+    public CoursesController(CityService cityService, ValueService valueService, TypeService typeService,
+                             BankService bankService, SelectService selectService, NationalBank nationalBank,
+                             BranchService branchService) {
+        this.cityService = cityService;
+        this.valueService = valueService;
+        this.typeService = typeService;
+        this.bankService = bankService;
+        this.selectService = selectService;
+        this.nationalBank = nationalBank;
+        this.branchService = branchService;
+    }
+
+    @GetMapping("/courses/{city}")
+    public ModelAndView coursesPage(@PathVariable("city") String city,
+                                    @RequestParam(defaultValue = "USD") String type) {
+        modelAndView.addObject("city", city);
+        modelAndView.addObject("type", type);
+
+        City selectedCity = cityService.getByName(city);
+        List<City> cities = cityService.getAll();
+        List<TypeCurrency> types = typeService.getAll();
+        List<BankBranch> filials = branchService.getAll();
+        Set<Bank> banks = CoursesUtil.listBanks(filials, selectedCity);
+        modelAndView.addObject("cities", cities);
+        modelAndView.addObject("banks", banks);
+        modelAndView.addObject("types", types);
+
+        TypeCurrency typeCurrency = typeService.getByName(type);
+        List<ValueCurrency> values = valueService.getAll();
+        List<String> valuesSell = new ArrayList<>();
+        List<String> valuesBuy = new ArrayList<>();
+        for (Bank bank : banks) {
+            for (BankBranch branch : CoursesUtil.filialsCityAndBank(bank, selectedCity, filials)) {
+                ValueCurrency valueSell = CoursesUtil.getValue(values, branch, typeCurrency, "Продажа");
+                ValueCurrency valueBuy = CoursesUtil.getValue(values, branch, typeCurrency, "Покупка");
+                if (branch.equals(valueSell.getBranch())) {
+                    valuesSell.add(CoursesUtil.getCorrectValue(valueSell));
+                } else if (branch.equals(valueBuy.getBranch())) {
+                    valuesBuy.add(CoursesUtil.getCorrectValue(valueBuy));
+                }
+            }
+        }
+        modelAndView.addObject("valuesSell", valuesSell);
+        modelAndView.addObject("valuesBuy", valuesBuy);
+
+        if (type.equalsIgnoreCase("usd")) {
+            modelAndView.addObject("nbrb", nationalBank.getValueUSD());
+        } else if (type.equalsIgnoreCase("eur")) {
+            modelAndView.addObject("nbrb", nationalBank.getValueEUR());
+        } else if (type.equalsIgnoreCase("rub")) {
+            modelAndView.addObject("nbrb", nationalBank.getValueRUB());
+        }
         modelAndView.setViewName("courses");
         return modelAndView;
     }
