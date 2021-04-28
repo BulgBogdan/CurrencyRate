85cfb4e06b1645e541cc1989c2a8461cc584a5c7
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-04-28 22:13:29 +0300

    correct banks controller

--- src/main/java/currencyRate/controller/BanksController.java
+++ src/main/java/currencyRate/controller/BanksController.java
@@ -1,16 +1,41 @@
 package currencyRate.controller;
 
+import currencyRate.controller.utils.BanksUtil;
+import currencyRate.entity.TypeCurrency;
+import currencyRate.entity.ValueCurrency;
+import currencyRate.service.TypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class BanksController {
 
     private ModelAndView modelAndView = new ModelAndView();
 
-    @GetMapping("/banks")
-    public ModelAndView banksPage() {
+    private TypeService typeService;
+
+    @Autowired
+    public BanksController(TypeService typeService) {
+        this.typeService = typeService;
+    }
+
+    @GetMapping("/banks/{city}/{bank}")
+    public ModelAndView banksPage(@PathVariable("city") String city,
+                                  @PathVariable("bank") String bank) {
+        List<TypeCurrency> types = typeService.getAll();
+        List<ValueCurrency> sells = BanksUtil.valuesSell(types, city, bank);
+        List<ValueCurrency> buys = BanksUtil.valuesBuy(types, city, bank);
+
+        modelAndView.addObject("sells", sells);
+        modelAndView.addObject("buys", buys);
+        modelAndView.addObject("types", types);
+        modelAndView.addObject("bank", bank);
+        modelAndView.addObject("city", city);
         modelAndView.setViewName("banks");
         return modelAndView;
     }
