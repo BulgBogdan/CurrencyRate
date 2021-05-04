59cea442ffe08912455d9f988a2ed174960ae680
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-05-04 18:08:52 +0300

    correct calculate controller

--- src/main/java/currencyRate/controller/CalculateController.java
+++ src/main/java/currencyRate/controller/CalculateController.java
@@ -1,17 +1,78 @@
 package currencyRate.controller;
 
+import currencyRate.controller.utils.CalculatorUtil;
+import currencyRate.entity.City;
+import currencyRate.entity.SelectCurrency;
+import currencyRate.entity.TypeCurrency;
+import currencyRate.entity.ValueCurrency;
+import currencyRate.service.CityService;
+import currencyRate.service.SelectService;
+import currencyRate.service.TypeService;
+import currencyRate.service.ValueService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class CalculateController {
 
     private ModelAndView modelAndView = new ModelAndView();
 
-    @GetMapping("/calculate")
-    public ModelAndView calculatePage() {
+    private CityService cityService;
+
+    private ValueService valueService;
+
+    private TypeService typeService;
+
+    private SelectService selectService;
+
+    @Autowired
+    public CalculateController(CityService cityService, ValueService valueService,
+                               TypeService typeService, SelectService selectService) {
+        this.cityService = cityService;
+        this.valueService = valueService;
+        this.typeService = typeService;
+        this.selectService = selectService;
+    }
+
+    @GetMapping("/calculate/{city}")
+    public ModelAndView calculatePage(@PathVariable("city") String city) {
+        String value = "";
+        String typ = "";
+        String sel = "";
+        List<City> cities = cityService.getAll();
+        List<TypeCurrency> types = typeService.getAll();
+        List<ValueCurrency> values = CalculatorUtil.getCityValues(valueService.getAll(), city);
+        List<SelectCurrency> selects = selectService.getAll();
+        List<String> sells = CalculatorUtil.getBestValues("Продажа", types, values);
+        List<String> buys = CalculatorUtil.getBestValues("Покупка", types, values);
+        modelAndView.addObject("cities", cities);
+        modelAndView.addObject("selects", selects);
+        modelAndView.addObject("types", types);
+        modelAndView.addObject("sells", sells);
+        modelAndView.addObject("buys", buys);
+        modelAndView.addObject("city", city);
+        modelAndView.addObject("value", value);
+        modelAndView.addObject("typ", typ);
+        modelAndView.addObject("sel", sel);
         modelAndView.setViewName("calculate");
+        return modelAndView;
+    }
+
+    @PostMapping("/calculate/{city}")
+    public ModelAndView calculate(@PathVariable("city") String city,
+                                  @ModelAttribute("sel") String select,
+                                  @ModelAttribute("typ") String type,
+                                  @ModelAttribute("value") String value) {
+        String sel = select;
+        String typ = type;
+        String val = value;
         return modelAndView;
     }
 }
