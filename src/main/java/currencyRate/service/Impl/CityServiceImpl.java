5f4392c15e9effc0a4dc2eea61209d93a925a92d
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-06-07 21:13:58 +0300

    added log in city repository

--- src/main/java/currencyRate/service/Impl/CityServiceImpl.java
+++ src/main/java/currencyRate/service/Impl/CityServiceImpl.java
@@ -3,17 +3,21 @@ package currencyRate.service.Impl;
 import currencyRate.entity.City;
 import currencyRate.repository.CityRepository;
 import currencyRate.service.CityService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Transactional
 public class CityServiceImpl implements CityService {
 
     private CityRepository cityRepository;
+
+    private static final Logger logger = Logger.getLogger(CityServiceImpl.class);
 
     @Autowired
     public CityServiceImpl(CityRepository cityRepository) {
@@ -22,31 +26,64 @@ public class CityServiceImpl implements CityService {
 
     @Override
     public City add(City city) {
-        return cityRepository.save(city);
+        City createdCity = cityRepository.save(city);
+        if (Objects.nonNull(createdCity)) {
+            logger.info(createdCity + " successfully added. City : " + createdCity);
+        } else {
+            logger.error("Error, City not added");
+        }
+        return createdCity;
     }
 
     @Override
     public void delete(int id) {
         cityRepository.deleteById(id);
+        logger.info("City deleted.");
     }
 
     @Override
     public City getById(int id) {
-        return cityRepository.getById(id);
+        City getCity = cityRepository.getById(id);
+        if (Objects.nonNull(getCity)) {
+            logger.info(getCity + " successfully found. City : " + getCity);
+        } else {
+            logger.error("Error, City not found");
+        }
+        return getCity;
     }
 
     @Override
     public City getByName(String cityName) {
-        return cityRepository.getByName(cityName);
+        City getCity = cityRepository.getByName(cityName);
+        if (Objects.nonNull(getCity)) {
+            logger.info(getCity + " successfully found. City : " + getCity);
+        } else {
+            logger.error("Error, City not found");
+        }
+        return getCity;
     }
 
     @Override
     public City edit(City city) {
-        return cityRepository.saveAndFlush(city);
+        City editedCity = cityRepository.saveAndFlush(city);
+        if (Objects.nonNull(editedCity)) {
+            logger.info(editedCity + " successfully edited. City : " + editedCity);
+        } else {
+            logger.error("Error, City not edited");
+        }
+        return editedCity;
     }
 
     @Override
     public List<City> getAll() {
-        return cityRepository.findAll();
+        List<City> cities = cityRepository.findAll();
+        if (cities.isEmpty()) {
+            logger.error("Error, Cities not founded");
+        } else {
+            for (City city : cities) {
+                logger.info(city + " successfully founded. City : " + city);
+            }
+        }
+        return cities;
     }
 }
