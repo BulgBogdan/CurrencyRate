fdde0d26033810bca01351a0631637e518a59651
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-06-18 23:09:17 +0300

    added log value currency service

--- src/main/java/currencyRate/service/Impl/ValueServiceImpl.java
+++ src/main/java/currencyRate/service/Impl/ValueServiceImpl.java
@@ -3,18 +3,22 @@ package currencyRate.service.Impl;
 import currencyRate.entity.ValueCurrency;
 import currencyRate.repository.ValueRepository;
 import currencyRate.service.ValueService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Transactional
 public class ValueServiceImpl implements ValueService {
 
     private ValueRepository valueRepository;
+
+    private static final Logger logger = Logger.getLogger(ValueServiceImpl.class);
 
     @Autowired
     public ValueServiceImpl(ValueRepository valueRepository) {
@@ -23,27 +27,54 @@ public class ValueServiceImpl implements ValueService {
 
     @Override
     public ValueCurrency add(ValueCurrency valueCurrency) {
-        return valueRepository.saveAndFlush(valueCurrency);
+        ValueCurrency value = valueRepository.save(valueCurrency);
+        if (Objects.nonNull(value)) {
+            logger.info(value + " successfully added. ValueCurrency : " + value);
+        } else {
+            logger.error("Error, ValueCurrency not added");
+        }
+        return value;
     }
 
     @Override
     public void delete(int id) {
         valueRepository.deleteById(id);
+        logger.info("ValueCurrency deleted.");
     }
 
     @Override
     public ValueCurrency getById(int id) {
-        return valueRepository.getById(id);
+        ValueCurrency value = valueRepository.getById(id);
+        if (Objects.nonNull(value)) {
+            logger.info(value + " successfully found. ValueCurrency : " + value);
+        } else {
+            logger.error("Error, ValueCurrency not found");
+        }
+        return value;
     }
 
     @Override
     public ValueCurrency edit(ValueCurrency valueCurrency) {
-        return valueRepository.saveAndFlush(valueCurrency);
+        ValueCurrency value = valueRepository.saveAndFlush(valueCurrency);
+        if (Objects.nonNull(value)) {
+            logger.info(value + " successfully edit. ValueCurrency : " + value);
+        } else {
+            logger.error("Error, ValueCurrency not edit");
+        }
+        return value;
     }
 
     @Override
     public List<ValueCurrency> getAll() {
-        return valueRepository.findAll();
+        List<ValueCurrency> values = valueRepository.findAll();
+        if (values.isEmpty()) {
+            logger.error("Error, ValueCurrency not founded");
+        } else {
+            for (ValueCurrency value : values) {
+                logger.info(value + " successfully founded. ValueCurrency : " + value);
+            }
+        }
+        return values;
     }
 
     @Override
